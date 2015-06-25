c6ebd0d4c0b8b3e7da4c36992f12b891164e3ac8
SportySpice <sportyspice@outlook.com>
2015-06-26 02:46:46 +0300

    Source Color

--- src/collection/settings/FeedTextSettings.py
+++ src/collection/settings/FeedTextSettings.py
@@ -16,8 +16,10 @@ D_PLAY_ALL       = TextSettings         (None,          False,  False, show=Fals
 # D_VIDEO_COUNT  = CountTextSettings    ('yellow',      False,  False, Location.MIDDLE, CountType.VIEWS)
 # D_VIDEO_SOURCE = TextSettings         ('tan',         True,   True)
 # D_VIDEO_TITLE  = TextSettings         ('seashell',    False,  False)
+
 D_VIDEO_COUNT    = CountTextSettings    ('yellow',      False,  True, Location.MIDDLE, CountType.VIEWS)
-D_VIDEO_SOURCE   = TextSettings         ('chocolate',   False,  False)
+D_VIDEO_SOURCE   = TextSettings         ('red',         False,  False)
+#D_VIDEO_SOURCE  = TextSettings         ('chocolate',   False,  False)
 D_VIDEO_TITLE    = TextSettings         (None,          False,  False)
 
 D_USE = False
