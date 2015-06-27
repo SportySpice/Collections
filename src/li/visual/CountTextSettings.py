aa92eb4fd1c717589782b19152bc57003a940612
SportySpice <sportyspice@outlook.com>
2015-06-28 02:07:39 +0300

    Changed decimal mark function to one that works in all python versions
    Original function was much cleaner (only 1 line). Possibly also more
    efficient. However, it only works on python 2.7+ so it crashes on Kodi
    for Android, and possibly on other versions as well.

--- src/li/visual/CountTextSettings.py
+++ src/li/visual/CountTextSettings.py
@@ -20,13 +20,45 @@ class CountTextSettings(TextSettings):
         if not self.show:
             return ''
         
-        countText = '{:,}'.format(number)
+        #countText = '{:,}'.format(number)    # only works on python 2.7+
+        countText = intWithCommas(number)
         countText = '(%s)' % super(CountTextSettings, self).apply(countText)
         
         return countText
+
+
+def intWithCommas(number):
+    if type(number) not in [type(0), type(0L)]:
+        raise TypeError("Parameter must be an integer.")
+    if number < 0:
+        return '-' + intWithCommas(-number)
+    result = ''
+    while number >= 1000:
+        number, r = divmod(number, 1000)
+        result = ",%03d%s" % (r, result)
+    return "%d%s" % (number, result)
+
+
+# def intWithCommas2(number):
+#     s = '%d' % number
+#     groups = []
+#     while s and s[-1].isdigit():
+#         groups.append(s[-3:])
+#         s = s[:-3]
+#     return s + ','.join(reversed(groups))
+
+
+
+
     
     
     
 def fromOther(countTS):
     c = countTS
     return CountTextSettings(c.color, c.bold, c.italic, c.location, c.countType, c.show)
+
+
+
+
+
+
