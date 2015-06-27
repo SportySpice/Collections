596b2285d6de21c1737b1add3809e35a99c8499a
SportySpice <sportyspice@outlook.com>
2015-06-28 02:21:12 +0300

    Can now debug on android devices! (emulators & physical)
    Woohoo!

--- src/tools/debug.py
+++ src/tools/debug.py
@@ -2,5 +2,8 @@ from pysrc import pydevd
 import logging
 
 def start():
-    pydevd.settrace('localhost', stdoutToServer=True, stderrToServer=True, suspend=False)        
+    pydevd.settrace('localhost', stdoutToServer=True, stderrToServer=True, suspend=False)
+    #pydevd.settrace('192.168.1.10', stdoutToServer=True, stderrToServer=True, suspend=False)
+    #pydevd.settrace('192.168.1.10')        
+    
     logging.basicConfig(level=logging.DEBUG)
