3b20d718b3f9d5fa0bc4a66ee5f36c1c8532516c
SportySpice <sportyspice@outlook.com>
2015-06-28 01:50:14 +0300

    added pythonpath in tools folder
    added pythonpath in tools folder, can now import libraries without
    modifying them.

--- addon.py
+++ addon.py
@@ -1,10 +1,19 @@
+import xbmc
 import sys
 from src import router
+
+
+sys.path.append(xbmc.translatePath('special://home/addons/plugin.video.collections/src/tools'))
 
 #path = sys.argv[0][34:-1]
 #path = sys.argv[0][:-1]
 path = sys.argv[0]
 query = sys.argv[2][1:]
-
   
 router.route(path, query)
+
+
+
+
+
+
